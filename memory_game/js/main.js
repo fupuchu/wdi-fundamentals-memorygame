0fca87bd3b8bd23209dd421288f9b63a093cd176
Leon Ho <fupuchu@Leons-MacBook-Pro.local>
2018-05-01 04:10:18 +0800

    End of Unit 8

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,19 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "King";
+var cards = ["queen","queen","king","king"];
 
-console.log(cardOne);
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+  console.log("User flipped" + " " + cardOne);
+  console.log("User flipped" + " " + cardTwo);
+  if (cardOne === cardTwo) {
+    alert("You found a match");
+  }else {
+    alert("Keep trying");
+  }
+}
